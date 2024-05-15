2cb0902602ab95441ade25e4fe5e552913ba1af4
BioTech4Env <biotech.4env@gmail.com>
2024-05-15 08:59:58 +0200

    Build pseudo file editor

--- CustomCLI/Program.cs
+++ CustomCLI/Program.cs
@@ -3,6 +3,7 @@
 Console.WriteLine("""
     CLI tools from scratch by BioTech4Env
     Made to refine C# skills
+    Type "help" (without quotation marks) to get an overview of the current working commands
 
     """);
 
