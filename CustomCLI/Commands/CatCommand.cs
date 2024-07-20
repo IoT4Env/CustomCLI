248423ddd06fdbc4e65b44f7dd65dfcf76dfc5a4
IoT4Env <ioan.matteo253@gmail.com>
2024-07-20 20:30:33 +0200

    Added cat command inside dedicated class

---
+++ CustomCLI/Commands/CatCommand.cs
@@ -0,0 +1,36 @@
+﻿using static CustomCLI.Kernel;
+using CustomCLI.Commands.ICommands;
+
+namespace CustomCLI.Commands;
+
+public class CatCommand : ICommandComposite
+{
+    /// <summary>
+    /// Tracks the specific file present somewhere in the file system and checks if the file exists
+    /// </summary>
+    /// <param name="compositePath">file name or path-to-file string</param>
+    /// <returns>execution permission</returns>
+    public static bool CanExecute(CompositePath compositePath)
+    {
+        var offset = Tree.Count + compositePath.ArgsNum - 2;
+        VirtualFile dir = GetFileByPosition(compositePath.LastArgName, offset);
+
+        if (dir is null)
+        {
+            Console.WriteLine($"No such file: {compositePath.LastArgName}");
+            return false;
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// Prints the content of the specified file
+    /// </summary>
+    /// <param name="compositePath">file name or path-to-file string</param>
+    public static void Execute(CompositePath compositePath)
+    {
+        var offset = Tree.Count + compositePath.ArgsNum - 2;
+        var file = Dirs[offset].Files.FirstOrDefault(r => r.Name.Equals(compositePath.LastArgName));
+        Console.WriteLine(file.Content);
+    }
+}
