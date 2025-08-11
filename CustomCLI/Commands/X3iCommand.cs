11a77d498a62b976ad8a8fa2dae094121a82d53c
IoT4Env <170166502+IoT4Env@users.noreply.github.com>
2025-08-11 13:20:48 +0200

    Added the x3i command + minor bug fixes

---
+++ CustomCLI/Commands/X3iCommand.cs
@@ -0,0 +1,47 @@
+﻿using CustomCLI.Commands.ICommands;
+using static CustomCLI.Kernel;
+
+namespace CustomCLI.Commands;
+
+public class X3iCommand : ICommandComposite
+{
+    /// <summary>
+    /// Tracks the specific file present somewhere in the file system and checks if the file exists
+    /// </summary>
+    /// <param name="compositePath">file name or path-to-file string</param>
+    /// <returns>true if the file exists and if the file extension is correct</returns>
+    public static bool CanExecute(CompositePath compositePath)
+    {
+        var offset = Tree.Count + compositePath.ArgsNum - 2;
+        VirtualFile file = GetFileByPosition(compositePath.LastArgName, offset);
+        if (file is null)
+        {
+            Console.WriteLine($"No such file: {compositePath.LastArgName}");
+            return false;
+        }
+
+        var splittedFile = file.Name.Split('.');
+        var fileExtension = splittedFile[splittedFile.Length - 1];
+        if (!fileExtension.Equals("x3i"))
+        {
+            Console.WriteLine($"Unhandled file extension for x3i command: .{fileExtension}");
+            return false;
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// Executes all lines inside the file as if they where written in the console by the user.
+    /// It simulates the bash and sh executables behaviour.
+    /// </summary>
+    /// <param name="compositePath">file name or path-to-file string</param>
+    public static void Execute(CompositePath compositePath)
+    {
+        var offset = Tree.Count + compositePath.ArgsNum - 2;
+        var script = Dirs[offset].Files.FirstOrDefault(r => r.Name.Equals(compositePath.LastArgName));
+
+        var lines = script.Content.Split('\n');
+        foreach (var line in lines)
+            Kernel.Execute(line.Split(' '));
+    }
+}
