246c19bcf93f17270bd3562c38479efd5c8d972e
IoT4Env <170166502+IoT4Env@users.noreply.github.com>
2025-08-11 13:20:48 +0200

    Created file for echo command options

--- CustomCLI/CliCommands/EchoCommand.cs
+++ CustomCLI/CliCommands/EchoCommand.cs
@@ -1,6 +1,6 @@
-﻿using CustomCLI.CliCommands.Resources;
+﻿using CustomCLI.CliCommands;
+using CustomCLI.CliCommands.Resources;
 using CustomCLI.Commands.ICommands;
-using System.Text.RegularExpressions;
 
 namespace CustomCLI.Commands;
 
@@ -21,7 +21,7 @@ public class EchoCommand : ICommand
         }
 
         var purifiedArgs = PurifyArgs(args);
-        var syntax = EchoCommandOptions.CheckSyntax(purifiedArgs);
+        var syntax = EchoCommandOption.CheckSyntax(purifiedArgs);
 
         if (syntax is null)
         {
@@ -60,8 +60,8 @@ public class EchoCommand : ICommand
             return;
         }
 
-        if (EchoCommandOptions.CanExecute(syntax))
-            EchoCommandOptions.Execute(syntax);
+        if (EchoCommandOption.CanExecute(syntax))
+            EchoCommandOption.Execute(syntax);
     }
 
     private static void Echo(string arg) => Console.WriteLine(arg);
@@ -85,66 +85,3 @@ public class EchoCommand : ICommand
     }
 }
 
-class EchoCommandOptions : ICommand
-{
-    /// <summary>
-    /// A string whose chars are associated with options of the echo command
-    /// </summary>
-    private static readonly string Options = "neE";
-
-    public static bool CanExecute(CommandSyntax syntax)
-    {
-        if (syntax.Option is null)
-        {
-            Console.WriteLine($"Argument cannot be null");
-            return false;
-        }
-
-        if (syntax.Option.Length > Options.Length)
-        {
-            Console.WriteLine("Too many arguments");
-            return false;
-        }
-
-        foreach (char option in syntax.Option)
-        {
-            if (!Options.Contains(option))
-            {
-                Console.WriteLine($"Invalid command option: {option}");
-                return false;
-            }
-        }
-
-        if (syntax.Option.Contains('e') && syntax.Option.Contains('E'))
-        {
-            Console.WriteLine($"Arguments 'e' and 'E' cannot be specified together");
-            return false;
-        }
-
-        return true;
-    }
-
-    public static CommandSyntax? CheckSyntax(string[] args)
-        => args[0].Contains('-') && args.Length > 1
-            ? new CommandSyntax
-            {
-                Arg = string.Join(" ", args[1..args.Length]),
-                Option = args[0].Replace("-", "")
-            }
-            : null;
-
-    public static void Execute(CommandSyntax syntax)
-    {
-        var output = syntax.Arg;
-        if (syntax.Option.Contains('e'))
-        {
-            output = Regex.Unescape(syntax.Arg);
-        }
-        if (syntax.Option.Contains('n'))
-        {
-            Console.Write(output);
-            return;
-        }
-        Console.WriteLine(output);
-    }
-}
---
+++ CustomCLI/CliCommands/EchoCommandOption.cs
@@ -0,0 +1,67 @@
+﻿using CustomCLI.Commands.ICommands;
+
+namespace CustomCLI.CliCommands;
+
+public class EchoCommandOption : ICommand
+{
+    /// <summary>
+    /// A string whose chars are associated with options of the echo command
+    /// </summary>
+    private static readonly string Options = "neE";
+
+    public static bool CanExecute(CommandSyntax syntax)
+    {
+        if (syntax.Option is null)
+        {
+            Console.WriteLine($"Argument cannot be null");
+            return false;
+        }
+
+        if (syntax.Option.Length > Options.Length)
+        {
+            Console.WriteLine("Too many arguments");
+            return false;
+        }
+
+        foreach (char option in syntax.Option)
+        {
+            if (!Options.Contains(option))
+            {
+                Console.WriteLine($"Invalid command option: {option}");
+                return false;
+            }
+        }
+
+        if (syntax.Option.Contains('e') && syntax.Option.Contains('E'))
+        {
+            Console.WriteLine($"Arguments 'e' and 'E' cannot be specified together");
+            return false;
+        }
+
+        return true;
+    }
+
+    public static CommandSyntax? CheckSyntax(string[] args)
+        => args[0].Contains('-') && args.Length > 1
+            ? new CommandSyntax
+            {
+                Arg = string.Join(" ", args[1..args.Length]),
+                Option = args[0].Replace("-", "")
+            }
+            : null;
+
+    public static void Execute(CommandSyntax syntax)
+    {
+        var output = syntax.Arg;
+        if (syntax.Option.Contains('e'))
+        {
+            output = Regex.Unescape(syntax.Arg);
+        }
+        if (syntax.Option.Contains('n'))
+        {
+            Console.Write(output);
+            return;
+        }
+        Console.WriteLine(output);
+    }
+}
