240f7f84a6dbdaeabf35cbb0f9248370e67c4898
IoT4Env <170166502+IoT4Env@users.noreply.github.com>
2025-08-11 13:20:47 +0200

    First attempt of implementing the command design pattern

---
+++ CustomCLI/Commands/EchoCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomCLI.Commands;
+
+public class EchoCommand : ICommand
+{
+    public static bool CanExecute(string arg)
+    {
+        return true;
+    }
+
+    public static void Execute(string arg) => Console.WriteLine(arg);
+}
